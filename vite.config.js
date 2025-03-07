627be839386769315a943ccebd5cd2786d772dfd
medtijabou <tijarim7@gmail.com>
2025-03-07 12:07:51 +0100

    package_json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
   plugins: [react()],
+  // Assure-toi que le base path est correctement configuré pour la production
+  base: process.env.NODE_ENV === 'production' ? '/P7_Kasa/' : '/',
 })

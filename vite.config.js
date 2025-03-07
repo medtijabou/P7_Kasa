890b3e95c28c9759ba7a757e280763e8c8dea9c3
medtijabou <tijarim7@gmail.com>
2025-03-07 12:38:27 +0100

    mise sur site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/P7_Kasa/',  // Change ici pour correspondre au nom de ton projet GitHub
+  base: '/P7_Kasa/',  // Chemin relatif pour GitHub Pages
 })

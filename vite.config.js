2b2a58385da53c52ec61151187431c97abe81aa0
medtijabou <tijarim7@gmail.com>
2025-03-07 13:07:51 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
-  base: '/P7_Kasa/',
+  base: '/P7_Kasa/', // Assurez-vous que la base est correcte pour GitHub Pages
 })

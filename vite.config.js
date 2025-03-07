c807408edb20da874b4ad28c17e936b3ed507045
medtijabou <tijarim7@gmail.com>
2025-03-07 12:20:31 +0100

    debuguer mon site

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Assure-toi que le base path est correctement configuré pour la production
-  base: process.env.NODE_ENV === 'production' ? '/P7_Kasa/' : '/',
+  base: '/P7_Kasa/',  // Assurez-vous que le chemin de base correspond à votre nom de repository
 })

5f714c4f47e53b473d332d2117996575ecb4f187
medtijabou <tijarim7@gmail.com>
2025-03-10 10:23:14 +0100

    scss hogar

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
+// filepath: /home/tijarim7/projet_open_class/projet_7_react/kasa/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/P7_Kasa/", // 🔹 Ajoute ceci pour GitHub Pages
   plugins: [react()],
   server: {
     historyApiFallback: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx"], // 🔹 Accepte les fichiers .jsx
-  },
-  esbuild: {
-    loader: "jsx", // 🔹 Assure la bonne gestion du JSX
   },
 });

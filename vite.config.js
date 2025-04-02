8d4d2fe64c441e41abfaf49f773aa056699757c5
medtijabou <tijarim7@gmail.com>
2025-04-02 13:06:18 +0200

    babel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-// filepath: /home/tijarim7/projet_open_class/projet_7_react/kasa/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"https://medtijabou.github.io/P7_Kasa/",
-  server: {
-    historyApiFallback: true,
-  },
 });
